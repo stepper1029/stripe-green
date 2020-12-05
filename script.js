7318b02d69fb34a7d99dcc40be3127216c4162dc
stepper1029 <69130851+stepper1029@users.noreply.github.com>
2020-12-05 15:25:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+/************************************************/
+/********* ADD CHECKLIST FUNCTIONALITY **********/
+/************************************************/
+
+var fullItemList = document.getElementById("my-list");
+
+if (fullItemList) {
+fullItemList.addEventListener("click", checkOffItem);
+}
+
+function checkOffItem(clicked) {
+    if (clicked.target.tagName == "LI") {
+      clicked.target.classList.toggle("all-done");
+
+    }
+}
+/* TODO: Add a variable to store the "my-list" element */
+
+
+/* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
+
+
+/* TODO: Declare the function checkOffList and add actions inside the { } */
+
+
